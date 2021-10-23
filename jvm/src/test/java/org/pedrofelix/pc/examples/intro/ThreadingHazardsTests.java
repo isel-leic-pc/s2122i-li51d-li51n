a78d456ce9b86d73f2146ea8980b1dc085b4feb9
Pedro Felix <pmhsfelix@gmail.com>
2021-10-23 16:57:50 +0100

    adds requirements to the ThreadingHazardsTests

--- jvm/src/test/java/org/pedrofelix/pc/examples/intro/ThreadingHazardsTests.java
+++ jvm/src/test/java/org/pedrofelix/pc/examples/intro/ThreadingHazardsTests.java
@@ -1,5 +1,6 @@
 package org.pedrofelix.pc.examples.intro;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.pedrofelix.pc.utils.TestUtils;
 
@@ -10,6 +11,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assume.assumeTrue;
 
 public class ThreadingHazardsTests {
 
@@ -18,11 +20,19 @@ public class ThreadingHazardsTests {
 
     private static final int N_OF_THREADS = 10;
 
+    @Before
+    public void checkRequirements() {
+        // These tests fail more frequently if running on system with only 1 processor (e.g. CI)
+        assumeTrue(Runtime.getRuntime().availableProcessors() > 1);
+    }
+
+
     /**************************************************************************
      * This test illustrates the problem of mutating a shared integer,
      * namely the loss of increments.
      */
     private int simpleCounter = 0;
+
     @Test
     public void loosing_increments() {
 
@@ -52,6 +62,7 @@ public class ThreadingHazardsTests {
      * which doesn't ensure atomicity of increments.
      */
     private volatile int volatileCounter = 0;
+
     @Test
     public void loosing_increments_even_with_volatile() {
 
@@ -77,6 +88,7 @@ public class ThreadingHazardsTests {
      * implemented using an AtomicInteger
      */
     private final AtomicInteger atomicCounter = new AtomicInteger();
+
     @Test
     public void not_loosing_increments_with_atomic() {
 
@@ -194,7 +206,7 @@ public class ThreadingHazardsTests {
     public void not_loosing_items_on_a_synchronized_list() {
 
         var ths = new ArrayList<Thread>(N_OF_THREADS);
-        for (int i = 0; i < N_OF_THREADS ; ++i) {
+        for (int i = 0; i < N_OF_THREADS; ++i) {
 
             var th = new Thread(() -> {
                 // producer
@@ -223,6 +235,7 @@ public class ThreadingHazardsTests {
      * the 'check' and the 'act'
      */
     private final Map<Integer, AtomicInteger> map = Collections.synchronizedMap(new HashMap<>());
+
     @Test
     public void loosing_increments_with_a_synchronized_map_and_atomics() {
 
